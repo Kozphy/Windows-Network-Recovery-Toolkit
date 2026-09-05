a086c5e28c5b31a2cea6130da32301bad2396f5d
Zixsa <50504962+Kozphy@users.noreply.github.com>
2026-09-05 18:55:34 +0800

    feat: export evidence-decision lifecycle models

--- src/platform_core/governance/__init__.py
+++ src/platform_core/governance/__init__.py
@@ -1,5 +1,15 @@
 from .chain_of_custody import chain_hash, verify_chain
 from .control_mapping import map_policy_outcome_to_controls
+from .evidence_decision_lifecycle import (
+    Claim,
+    ClaimStatus,
+    EvidenceDecisionLifecycle,
+    KnowledgeRecord,
+    UncertaintyAssessment,
+    VerificationResult,
+    VerificationStatus,
+    lifecycle_from_risk_decision,
+)
 from .evidence_to_action import (
     GOVERNANCE_MODEL,
     attach_governance_envelope,
@@ -10,12 +20,20 @@ from .evidence_to_action import (
 from .policy_compiler import compile_policy_matrix
 
 __all__ = [
+    "Claim",
+    "ClaimStatus",
+    "EvidenceDecisionLifecycle",
     "GOVERNANCE_MODEL",
+    "KnowledgeRecord",
+    "UncertaintyAssessment",
+    "VerificationResult",
+    "VerificationStatus",
     "attach_governance_envelope",
     "build_governance_envelope",
     "causal_language_allowed",
     "chain_hash",
     "compile_policy_matrix",
+    "lifecycle_from_risk_decision",
     "map_policy_outcome_to_controls",
     "narrative_passes_governance_language",
     "verify_chain",
